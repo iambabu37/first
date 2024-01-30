fc02440e34b3710a8516abe8f71ce99922b05ca6
iambabu37 <babukanthasamy333777@gmail.com>
2024-01-30 22:08:52 +0530

    thirty first

--- myProject/views.py
+++ myProject/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
+from django.db.models import Q
 from myapp.models import *
 from myapp.forms import *
+from django.contrib import messages
 
 def about(request):
     return render(request,"my_app/about.html")
@@ -24,15 +26,28 @@ def help(request):
     return render(request,"my_app/help.html")
 
 def search(request):
-    var = request.GET.get("main_search")
+    
+    var = request.GET.get("main_search") if request.GET.get != None else " "
     print(var)
-    dict = Plant.objects.all()
-    print(dict)
+    if not var or var.isspace():
+        messages.error(request, 'Please enter a valid search term.')
+        return render(request, "my_app/search.html")
+    
+    dict = Plant.objects.filter(
+         Q(name__icontains = var) &
+         Q(name__istartswith =var) 
+     )
+   
+    if not dict:
+        messages.info(request, 'No results found.')
+    
     content = {"dicts":dict}
-    print(content)
 
     return render(request,"my_app/search.html",content)
 
-def compound_detail(request):
-    return render (request,"my_app/compound_deatil.html")
+def compound_detail(request,name):
+    
+    dict = Plant.objects.filter(name=name)
+    content ={"dict":dict}
+    return render (request,"my_app/compound_detail.html",content)
 
