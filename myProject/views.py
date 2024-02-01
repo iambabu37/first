2ed55bc30ba4a23f2af86f936bc98ec440fb20d1
iambabu37 <babukanthasamy333777@gmail.com>
2024-02-01 23:14:10 +0530

    fpir first

--- myProject/views.py
+++ myProject/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from myapp.models import *
 from myapp.forms import *
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def about(request):
     return render(request,"my_app/about.html")
@@ -27,23 +28,38 @@ def help(request):
 
 def search(request):
     
-    var = request.GET.get("main_search") if request.GET.get != None else " "
-    print(var)
-    if not var or var.isspace():
-        messages.error(request, 'Please enter a valid search term.')
-        return render(request, "my_app/search.html")
-    
-    dict = Plant.objects.filter(
-         Q(name__icontains = var) &
-         Q(name__istartswith =var) 
-     )
-   
+    # var = request.GET.get("main_search") if request.GET.get is not None else " "
+    # print(var)
+    # if not var or var.isspace():
+    #     messages.error(request, 'Please enter a valid search term.')
+    #     return render(request, "my_app/search.html")
+    dict = Plant.objects.all().order_by("name")
+    # dict = Plant.objects.filter(
+    #      Q(name__icontains = var) &
+    #      Q(name__istartswith =var) 
+    #  )
+    print(dict)
     if not dict:
         messages.info(request, 'No results found.')
     
-    content = {"dicts":dict}
+    paginator = Paginator(dict, 10)  # Show 10 plants per page
+    page_number = request.GET.get('page')
+    final_data = paginator.get_page(page_number)
 
-    return render(request,"my_app/search.html",content)
+    # try: 
+    #     plant_page = paginator.page(page)
+    # except PageNotAnInteger:
+    #     plant_page = paginator.page(1)
+    # except EmptyPage:
+    #     plant_page = paginator.page(paginator.num_pages)
+
+    content = {"dicts": final_data}
+
+    return render(request, "my_app/search.html", content)
+    
+    # content = {"dicts":dict}
+
+    # return render(request,"my_app/search.html",content)
 
 def compound_detail(request,name):
     
--- myapp/models.py
+++ myapp/models.py
@@ -16,15 +16,22 @@ def img_chem(request,filename):
 
 class Phytochemical(models.Model):
     name = models.CharField(max_length=255)
-    struture = models.TextField()
-    molecular_weight = models.FloatField()
-    formula = models.CharField(max_length=255)
-    disease_association = models.CharField(max_length= 255)
-    bioactivity = models.CharField(max_length=255)
-    struture_image= models.ImageField(upload_to=img_chem)
-    plant_source = models.ForeignKey("plant",on_delete=models.CASCADE,related_name = "phyto")
-    description_compound = models.TextField()
-    time_of_add = models.DateTimeField(auto_now_add= True)
+    synonymous_names = models.TextField()
+    external_identifiers = models.TextField()
+    smiles = models.TextField()
+    inchi = models.TextField()
+    inchikey = models.TextField()
+    deepsmiles = models.TextField()
+    functional_groups = models.TextField()
+    classyfire_kingdom = models.CharField(max_length=255)
+    classyfire_superclass = models.CharField(max_length=255)
+    classyfire_class = models.CharField(max_length=255)
+    classyfire_subclass = models.CharField(max_length=255)
+    np_classifier_biosynthetic_pathway = models.TextField()
+    np_classifier_superclass = models.CharField(max_length=255)
+    np_classifier_class = models.CharField(max_length=255)
+    np_likeness_score = models.FloatField()
+    
    
     def __str__(self):
         return self.name
@@ -44,6 +51,19 @@ class Plant(models.Model):
     def __str__(self):
         return self.name
 
+class DruglikenessProperties(models.Model):
+    phytochemical = models.OneToOneField(Phytochemical, on_delete=models.CASCADE)
+    lipinski_violations = models.IntegerField()
+    lipinski_rule = models.IntegerField()
+    ghose_violations = models.IntegerField()
+    veber_rule = models.CharField()
+    ghose_rule = models.CharField()
+    gsk_4_400_rule = models.BooleanField()
+    pfizer_3_75_rule = models.BooleanField()
+    qedw_score = models.FloatField()
+
+    def __str__(self):
+        return " DrukLikeness "
 
 class Contact(models.Model):
     name = models.CharField(max_length=50)
@@ -55,4 +75,56 @@ class Contact(models.Model):
     def __str__(self):
         return self.name
 
+class MolecularProperties(models.Model):
+    molecular_weight = models.FloatField()
+    log_p = models.FloatField()
+    topological_polar_surface_area = models.FloatField()
+    num_hydrogen_bond_acceptors = models.IntegerField()
+    num_hydrogen_bond_donors = models.IntegerField()
+    num_carbon_atoms = models.IntegerField()
+    num_heavy_atoms = models.IntegerField()
+    num_heteroatoms = models.IntegerField()
+    num_nitrogen_atoms = models.IntegerField()
+    num_sulfur_atoms = models.IntegerField()
+    num_chiral_carbon_atoms = models.IntegerField()
+    stereochemical_complexity = models.FloatField()
+    num_sp_hybridized_carbon_atoms = models.IntegerField()
+    num_sp2_hybridized_carbon_atoms = models.IntegerField()
+    num_sp3_hybridized_carbon_atoms = models.IntegerField()
+    shape_complexity = models.FloatField()
+    num_rotatable_bonds = models.IntegerField()
+    num_aliphatic_carbocycles = models.IntegerField()
+    num_aliphatic_heterocycles = models.IntegerField()
+    num_aliphatic_rings = models.IntegerField()
+    num_aromatic_carbocycles = models.IntegerField()
+    num_aromatic_heterocycles = models.IntegerField()
+    num_aromatic_rings = models.IntegerField()
+    total_num_rings = models.IntegerField()
+    num_saturated_carbocycles = models.IntegerField()
+    num_saturated_heterocycles = models.IntegerField()
+    num_saturated_rings = models.IntegerField()
+    num_sssr = models.IntegerField()
 
+    def __str__(self):
+        return f"Molecular Properties for compound ID: {self.id}"
+
+from django.db import models
+
+class ADMEProperties(models.Model):
+    bioavailability_score = models.FloatField()
+    solubility_class_esol = models.CharField(max_length=255)
+    solubility_class_silicos_it = models.CharField(max_length=255)
+    blood_brain_barrier_permeation = models.BooleanField()
+    gastrointestinal_absorption = models.BooleanField()
+    log_kp_skin_permeation = models.FloatField()
+    num_pains_structural_alerts = models.IntegerField()
+    num_brenk_structural_alerts = models.IntegerField()
+    cyp1a2_inhibitor = models.BooleanField()
+    cyp2c19_inhibitor = models.BooleanField()
+    cyp2c9_inhibitor = models.BooleanField()
+    cyp2d6_inhibitor = models.BooleanField()
+    cyp3a4_inhibitor = models.BooleanField()
+    p_glycoprotein_substrate = models.BooleanField()
+
+    def __str__(self):
+        return f"ADME Properties for compound ID: {self.id}"
